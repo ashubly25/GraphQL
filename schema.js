aea009ed55cf623a53b559019993d0da554eb2aa
ashubly25 <ashutosh2564@gmail.com>
2018-08-21 23:52:57 +0530

    Added Mutation

--- schema.js
+++ schema.js
@@ -56,16 +56,10 @@ const mutation = new GraphQLObjectType({
 		addCustomer:{
 			type:CustomerType,
 			args:{
-				name: {type: new GraphQLNonNull(GraphQLString)},
-				email: {type: new GraphQLNonNull(GraphQLString)},
-				age: {type: new GraphQLNonNull(GraphQLInt)}
-			}
+				id:{type: new GraphQLNonNull(GraphQLString)}
+				},
 			resolve(parentValue, args){
-				return axios.post('http://localhost:3000/customers', {
-					name:args.name,
-					email: args.email,
-					age:args.age
-				})
+				return axios.post('http://localhost:3000/customers'+args.id)
 				.then(res => res.data);
 			}
 		}
