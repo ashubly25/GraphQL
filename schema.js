ca2daf603123ab7a7e6d349c2d3596ed177fd8ca
ashubly25 <ashutosh2564@gmail.com>
2018-08-27 01:55:50 +0530

    append-mutation

--- schema.js
+++ schema.js
@@ -68,9 +68,37 @@ const mutation = new GraphQLObjectType({
 				})
 				.then(res => res.data);
 			}
-		}
+		},
+			deleteCustomer:{
+			type:CustomerType,
+			args:{
+				id:{type: new GraphQLNonNull(GraphQLString)}
+				},
+			resolve(parentValue, args){
+				return axios.post('http://localhost:3000/customers'+args.id,{
+					name:args.name,
+					email:args.email,
+					age:args.age
+				})
+				.then(res => res.data);
+			}
+		},
+		editCustomer:{
+			type:CustomerType,
+			args:{
+				id:{type: new GraphQLNonNull(GraphQLString)}
+				name: {type: GraphQLString},
+				email: {type: GraphQLString},
+				age: {type: GraphQLInt},
+				},
+			resolve(parentValue, args){
+				return axios.patch('http://localhost:3000/customers/'+args.id, args)
+				.then(res => res.data);
+			}
+		},
 	}
-})
+	}
+});
 
 module.exports = new GraphQLSchema({
 	query: RootQuery
