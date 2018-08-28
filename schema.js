ac2be6529c8152f73bd43b9f7894dcab9ae1a8a9
ashubly25 <ashutosh2564@gmail.com>
2018-08-28 23:54:13 +0530

    Mutations

--- schema.js
+++ schema.js
@@ -96,19 +96,6 @@ const mutation = new GraphQLObjectType({
 				.then(res => res.data);
 			}
 		},
-		mitigateCustomer:{
-			type:CustomerType,
-			args:{
-				id:{type: new GraphQLNonNull(GraphQLString)}
-				name: {type: GraphQLString},
-				email: {type: GraphQLString},
-				age: {type: GraphQLInt},
-				},
-			resolve(parentValue, args){
-				return axios.patch('http://localhost:3000/customers/'+args.id, args)
-				.then(res => res.data);
-			}
-		},
 	}
 	}
 });
